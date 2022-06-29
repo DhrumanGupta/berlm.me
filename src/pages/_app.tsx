2121ad3f6ff044ae80319e4927db609397277b91
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2022-06-30 01:54:32 +0530

    Add insights for production

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,19 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Navbar from "../components/Navbar";
 import { Theme } from "../types";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme={Theme.Dark}>
+      {process.env.NODE_ENV !== "development" && (
+        <>
+          <Script src="https://getinsights.io/js/insights.js"></Script>
+          <Script id="insights">
+            {`insights.init("HNbdPNIpAloCk8kC"); insights.trackPages();`}
+          </Script>
+        </>
+      )}
       <Navbar />
       <Component {...pageProps} />
     </ThemeProvider>
