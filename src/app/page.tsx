01cab26600785ebf4ff7d56d6ba8a72cd5666f53
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2024-11-09 23:17:12 +0530

    2nd year BRR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import type { NextPage } from "next";
-import PageHeader from "@/components/typography/PageHeader";
-import { linkedInUrl } from "@/lib/constants";
 import SchemaData from "@/components/SchemaData";
 import HeroImage from "@/components/home/HeroImage";
+import PageHeader from "@/components/typography/PageHeader";
+import { linkedInUrl } from "@/lib/constants";
 import { getPlaceholderLocal } from "@/lib/getPlaceholder";
+import type { NextPage } from "next";
 // import Quotes from "@/data/qotw.json";
-import Link from "@/components/Link";
 
 const Images: HeroImage[] = [
   { src: "/hero0.webp", alt: "A picture of me next to a sunset", base64: "" },
@@ -54,7 +53,7 @@ const Home: NextPage = async () => {
         <article className="prose dark:prose-dark child:mb-2 last:child:mb-0">
           <p>
             Hi! I&apos;m Dhruman Gupta, a tech enthusiant and an aspiring
-            Software Engineer. I&apos;m a first-year at Ashoka University,
+            Software Engineer. I&apos;m a second-year at Ashoka University,
             studying Computer Science and Mathematics, currently working on
             personal and open source projects.
           </p>
